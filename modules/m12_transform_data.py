361e91b9808c646e616af33c048ec9eaee898341
6oncvlo <106825103+6oncvlo@users.noreply.github.com>
2024-05-16 16:37:03 +0000

    updated transform_data function from m12_transform_data

--- modules/m12_transform_data.py
+++ modules/m12_transform_data.py
@@ -1,7 +1,7 @@
 import modules.m11_load_data
 import pandas as pd
 
-def transform_data():
+def transform_data(only_ratings_set : bool = True):
     
     df_users, df_movies, df_ratings = modules.m11_load_data.load_data()
 
@@ -18,7 +18,10 @@ def transform_data():
     df_ratings['date'] = pd.to_datetime(df_ratings['timestamp'],unit='s').dt.date.astype('string').str.replace('-','')
     df_ratings['time'] = pd.to_datetime(df_ratings['timestamp'],unit='s').dt.time.astype('string').str.replace(':','')
 
-    return df_users, df_movies, df_ratings
+    if only_ratings_set:
+        return df_ratings
+    else:
+        return df_users, df_movies, df_ratings
 
 def matrix_ui():
     
