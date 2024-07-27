7b3245cecb4fa7d8bf8bb78c218cf8ac14bdc5ac
6oncvlo <106825103+6oncvlo@users.noreply.github.com>
2024-07-27 22:35:12 +0000

    - move config to main - move main_train directory origin - replace data loading and data preparation modules by new ones

--- src/data/data_loader.py
+++ src/data/data_loader.py
@@ -1,14 +1,30 @@
 import pandas as pd
+#from surprise import Dataset, Reader
 
-def load_data(config):
+def load_data(config:dict)->dict:
 
     dataframes=dict()
-    kwargs = {'encoding' : 'latin-1', 'header' : None}
+    kwargs = {'encoding':'latin-1', 'header':None}
     for df_name in list(config.keys()):
+        sep="|"
+        if df_name=='ratings':
+            sep="\t"
         dataframes[df_name]=pd.read_csv(
             filepath_or_buffer=config[df_name]['path']
             , names=config[df_name]['columns']
-            , sep="|"
+            , sep=sep
             , **kwargs 
         )
     return dataframes
+
+def load_from_df(df:pd.DataFrame):
+    """
+    Convert dataframe into readable object to use in surprise models.
+
+    Args:
+        df_ratings : pd.DataFrame with given ratings of users per movie
+    Returns   
+    """
+
+    reader = Reader(rating_scale = (0,5))
+    return Dataset.load_from_df(df[["userId", "itemId", "rating"]], reader)
---
+++ src/data/prepare_data.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+def prepare_data(dataframes:dict):
+
+    for df_name in list(dataframes.keys()):
+        
+        # col range is [0,len(col)]
+        for col in ['userId', 'itemId']:
+            if col in dataframes[df_name].columns:
+                dataframes[df_name][col]=dataframes[df_name][col] - 1
+    
+    # transform users dataframe
+    dataframes['users']=dataframes['users']\
+        .astype(dtype={'gender':'string', 'job':'string', 'zip_code':'string'})
+    
+    # transform items dataframe
+    dataframes['items']=dataframes['items']\
+        .drop(columns=['video_release_date'])\
+        .astype(dtype={'movie_title':'string', 'IMDb_URL':'string'})
+    
+    # transform ratings dataframe
+    for col, chr in zip(['date', 'time'], ['-', ':']):
+        dataframes['ratings'][col]=pd.to_datetime(dataframes['ratings']['timestamp'],unit='s').dt.time.astype('string').str.replace(chr,'')
+    
+    return dataframes
