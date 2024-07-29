7d3b1aee4d66a3ea1a8e5ee9b6af06d3f5fd62af
6oncvlo <106825103+6oncvlo@users.noreply.github.com>
2024-07-29 18:38:14 +0000

    adjust grid search function based on HoldOut class implementation

--- src/models/models.py
+++ src/models/models.py
@@ -1,8 +1,10 @@
-import yaml
-from surprise.model_selection import KFold, RepeatedKFold, GridSearchCV
 from surprise import KNNWithMeans, SVD, NMF, CoClustering
-from src.data.data_loader import load_from_df
+import yaml
+from src.models.cv_iterator import KFold, RepeatedKFold, HoldOut
+from sklearn.model_selection import TimeSeriesSplit
 import pandas as pd
+from src.data.data_loader import Dataset_custom
+from surprise.model_selection import GridSearchCV
 
 # define algorithm objects and read its param grid
 algo_classes={'KNNWithMeans':KNNWithMeans, 'SVD':SVD, 'NMF':NMF, 'CoClustering':CoClustering}
@@ -11,7 +13,9 @@ with open('src/models/algo_params.yml', 'r') as file:
 
 # define cross-validation iterator and its params
 cv_iterators={'KFold':KFold(n_splits=5, random_state=0, shuffle=False)
-              , 'RepeatedKFold':RepeatedKFold(n_splits=5, n_repeats=3, random_state=0)}
+              , 'RepeatedKFold':RepeatedKFold(n_splits=5, n_repeats=3, random_state=0)
+              , 'TimeSeriesSplit':TimeSeriesSplit(n_splits=5, max_train_size=None, test_size=None, gap=0)
+              , 'HoldOut':HoldOut(validation_date='19980312')}
 
 class grid_search():
 
@@ -23,7 +27,9 @@ class grid_search():
         self.return_train_measures=return_train_measures
     
     def fit(self, data:pd.DataFrame):
-
+        # prepare ingestion into surprise models
+        data=Dataset_custom.from_df(data)
+        # set grid search params and fit data
         gs=GridSearchCV(
             algo_class=self.algo_class
             , param_grid=self.param_grid
@@ -31,15 +37,12 @@ class grid_search():
             , cv=self.cv
             , return_train_measures=self.return_train_measures
             )
-        # prepare ingestion into surprise models
-        data=load_from_df(data)
-        
-        # fit data into grid search and return results
         gs.fit(data=data)
+
         cv_results=pd.DataFrame.from_dict(gs.cv_results)
         cv_results.drop(
             columns=[col for col in cv_results.columns if any(col.startswith(drop_col) for drop_col in ['mean_', 'std_', 'rank_', 'params'])]
             , inplace=True
             )
-        #cv_results.columns = cv_results.columns.str.replace('param_', "", regex=False)
+        #cv_results.columns=cv_results.columns.str.replace('param_', "", regex=False)
         return cv_results
